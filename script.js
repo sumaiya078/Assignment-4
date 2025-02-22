69018ad5fea4bacb979ac7168d6df428477bc687
sumaiya <sumaiyataamboli07@gmail.com>
2025-02-22 20:21:54 +0530

    path changed

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ document.addEventListener('DOMContentLoaded', () => {
     {
       id: 1,
       name: 'iphone 14',
-      category: 'Mobiles',
+      category: 'Mobile',
       price: 79999,
       img: 'iphone 14.jpg',
     },
     {
       id: 2,
       name: 'Samsung Galaxy S23 Ultra 5g',
-      category: 'mobiles',
+      category: 'mobile',
       price: 74999,
       img: 'Samsung galaxy.jpg',
     },
@@ -111,7 +111,7 @@ document.addEventListener('DOMContentLoaded', () => {
     {
       id: 15,
       name: 'Tablet',
-      category: 'mobiles',
+      category: 'mobile',
       price: 39999,
       img: 'Tablet.jpg',
     },
